b973cad1b2409926853b19c2cf4fc36869c6f776
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2025-03-05 14:54:09 +0545

    url edit

--- src/app/ai_goat/_components/Board.tsx
+++ src/app/ai_goat/_components/Board.tsx
@@ -54,7 +54,7 @@ export default function Board() {
     if (selectedPosition === null) {
       return [];
     }
-    
+
     let directions = [];
 
     if ((selectedPosition.x + selectedPosition.y) % 2 != 0) {
@@ -110,35 +110,35 @@ export default function Board() {
   const [selectedPosition, setSelectedPosition] = useState<any | null>(null);
 
   useEffect(() => {
-    setTimeout(()=>{
-    if (capturedGoats >= 5) {
-      playTigerWinSound();
-      setWinner("tiger_win");
-    } else {
-      let flag = true;
-      let tigerBlocked_now = 0;
-      console.log("Tiger Positions", tigerPositions);
-      tigerPositions.map(([x, y]) => {
-        let position = available_position(
-          { x, y, name: "tiger" },
-          board,
-          "tiger"
-        );
-        if (position.length > 0) {
-          flag = false;
-        } else {
-          tigerBlocked_now = tigerBlocked_now + 1;
+    setTimeout(() => {
+      if (capturedGoats >= 5) {
+        playTigerWinSound();
+        setWinner("tiger_win");
+      } else {
+        let flag = true;
+        let tigerBlocked_now = 0;
+        console.log("Tiger Positions", tigerPositions);
+        tigerPositions.map(([x, y]) => {
+          let position = available_position(
+            { x, y, name: "tiger" },
+            board,
+            "tiger"
+          );
+          if (position.length > 0) {
+            flag = false;
+          } else {
+            tigerBlocked_now = tigerBlocked_now + 1;
+          }
+        });
+
+        console.log("Tiger Blocked", tigerBlocked_now);
+        setTigerBlocked(tigerBlocked_now);
+
+        if (flag) {
+          setWinner("goat_win");
         }
-      });
-
-      console.log("Tiger Blocked", tigerBlocked_now);
-      setTigerBlocked(tigerBlocked_now);
-
-      if (flag) {
-        setWinner("goat_win");
       }
-    }
-  },1)
+    }, 1);
   }, [capturedGoats, board, tigerPositions, selectedPosition]);
 
   const updateBoard = (data: any) => {
@@ -146,46 +146,44 @@ export default function Board() {
       return;
     }
     if (Array.isArray(data[0]) && data[0].length === 2) {
-
-        // Case: [[2,2],[3,2]]
-        const [source, destination] = data;
-        setTurn("tiger");
-        setBoard((prevBoard) => {
-            const newBoard = [...prevBoard];
-            newBoard[source[0]][source[1]] = 0;
-            newBoard[destination[0]][destination[1]] = 1;
-            return newBoard;
-        });
-        playGoatMoveSound();
+      // Case: [[2,2],[3,2]]
+      const [source, destination] = data;
+      setTurn("tiger");
+      setBoard((prevBoard) => {
+        const newBoard = [...prevBoard];
+        newBoard[source[0]][source[1]] = 0;
+        newBoard[destination[0]][destination[1]] = 1;
+        return newBoard;
+      });
+      playGoatMoveSound();
     } else if (data.length === 2 && typeof data[0] === "number") {
-        const source = data;
-        console.log("Source", source);
-        setTurn("tiger");
-        setBoard((prevBoard) => {
-            const newBoard = [...prevBoard];
-            newBoard[source[0]][source[1]] = 1;
-            return newBoard;
-        });
-          playGoatMoveSound();
-          setTotalGoats((prevTotalGoats) => prevTotalGoats - 1);
+      const source = data;
+      console.log("Source", source);
+      setTurn("tiger");
+      setBoard((prevBoard) => {
+        const newBoard = [...prevBoard];
+        newBoard[source[0]][source[1]] = 1;
+        return newBoard;
+      });
+      playGoatMoveSound();
+      setTotalGoats((prevTotalGoats) => prevTotalGoats - 1);
     }
-};
-
-  
+  };
 
   const getBestMove = async () => {
     setLoading(true); // Start loading
     try {
-
-      const response = await axios.post("https://baghchal-api.vercel.app/get_moves_goat", {
-        board: board,
-        tigers: tigerPositions,
-        total_goats: totalGoats,
-        goats_on_board: 20 - totalGoats, // Goats already placed
-        capture_goat: capturedGoats,
-        turn: turn,
-      });
-  
+      const response = await axios.post(
+        "https://baghchal-api.vercel.app/get_moves_goat",
+        {
+          board: board,
+          tigers: tigerPositions,
+          total_goats: totalGoats,
+          goats_on_board: 20 - totalGoats, // Goats already placed
+          capture_goat: capturedGoats,
+          turn: turn,
+        }
+      );
 
       if (response.data.moves.length > 0) {
         setTurn("tiger");
@@ -421,69 +419,76 @@ export default function Board() {
       </div>
 
       <div className="absolute top-5 right-5 w-[22rem] h-[10rem] flex flex-col  bg-[url(/inf.png)]  bg-center bg-cover">
-      <div className="w-full flex justify-center gap-4 mt-6">
-        <div className="flex items-center gap-[4rem] mt-6 ">
-          <div className="flex items-center gap-2">
-            <div>
-              <Image 
-              src={'/tigercross.png'}
-              alt="tiger"
-              width={40}
-              height={40}
-              />
+        <div className="w-full flex justify-center gap-4 mt-6">
+          <div className="flex items-center gap-[4rem] mt-6 ">
+            <div className="flex items-center gap-2">
+              <div>
+                <Image
+                  src={"/tigercross.png"}
+                  alt="tiger"
+                  width={40}
+                  height={40}
+                />
+              </div>
+              <p className="text-xl text-[black] font-semibold ">
+                {tigerBlocked}
+              </p>
             </div>
-            <p className="text-xl text-[black] font-semibold ">{tigerBlocked}</p>
-          </div>
-          <div className="flex items-center gap-2">
-          <div>
-              <Image 
-              src={'/goatcross.png'}
-              alt="tiger"
-              width={40}
-              height={40}
-              />
+            <div className="flex items-center gap-2">
+              <div>
+                <Image
+                  src={"/goatcross.png"}
+                  alt="tiger"
+                  width={40}
+                  height={40}
+                />
+              </div>
+              <p className="text-xl text-[black] font-semibold">
+                {capturedGoats}
+              </p>
             </div>
-            <p className="text-xl text-[black] font-semibold">{capturedGoats}</p>
           </div>
-        </div>
         </div>
         <div className="w-[76%] flex justify-end gap-2 mt-2">
-            <Image src="/goat.png" alt="tiger" width={35} height={35} /> <span className="text-xl font-semibold">{20 -totalGoats}</span>
+          <Image
+            src="/goat.png"
+            alt="tiger"
+            width={35}
+            height={35}
+          />{" "}
+          <span className="text-xl font-semibold">{20 - totalGoats}</span>
         </div>
       </div>
 
-              <div className="absolute bottom-5 right-5 w-[17rem] h-[6rem] flex items-center bg-[url(/detail.png)]  bg-center bg-cover justify-center">
-                <div className="flex items-center gap-8 mt-6">
-                  {
-                    turn === "goat" ? (
-                      <div className="flex items-center gap-2 mb-4">
-                        <div className=" border-2 bg-green-700 h-[50px] w-[50px]  border-white rounded-full">
-                          <Image
-                            src="/goat.png"
-                            alt="tiger"
-                            width={50}
-                            height={50}
-                          />
-                        </div>
-                        <p className="text-xl text-[black] font-bold">Goat</p>
-                      </div>
-                    ) : (
-                      <div className="flex items-center gap-2 mb-4">
-                        <div className=" border-2 bg-red-700 h-[50px] w-[50px] border-white rounded-full">
-                          <Image
-                            src="/tiger.png"
-                            alt="tiger"
-                            width={50}
-                            height={50}
-                          />
-                        </div>
-                        <p className="text-xl text-[black] font-bold">Tiger</p>
-                      </div>
-                    )
-                  }
-        
-                </div>
+      <div className="absolute bottom-5 right-5 w-[17rem] h-[6rem] flex items-center bg-[url(/detail.png)]  bg-center bg-cover justify-center">
+        <div className="flex items-center gap-8 mt-6">
+          {turn === "goat" ? (
+            <div className="flex items-center gap-2 mb-4">
+              <div className=" border-2 bg-green-700 h-[50px] w-[50px]  border-white rounded-full">
+                <Image
+                  src="/goat.png"
+                  alt="tiger"
+                  width={50}
+                  height={50}
+                />
               </div>
+              <p className="text-xl text-[black] font-bold">Goat</p>
+            </div>
+          ) : (
+            <div className="flex items-center gap-2 mb-4">
+              <div className=" border-2 bg-red-700 h-[50px] w-[50px] border-white rounded-full">
+                <Image
+                  src="/tiger.png"
+                  alt="tiger"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <p className="text-xl text-[black] font-bold">Tiger</p>
+            </div>
+          )}
+        </div>
+      </div>
 
       <SoundToggler />
 
@@ -503,86 +508,84 @@ export default function Board() {
         </div>
       )}
 
-        {
-          (winner == 'tiger_win' || winner == 'goat_win') &&
-          <div  className="h-[100vh] w-[100vw]  absolute top-0 left-0 bg-black opacity-60 flex justify-center pt-[2rem] ">
+      {(winner == "tiger_win" || winner == "goat_win") && (
+        <div className="h-[100vh] w-[100vw]  absolute top-0 left-0 bg-black opacity-60 flex justify-center pt-[2rem] "></div>
+      )}
+
+      {(winner == "tiger_win" || winner == "goat_win") && (
+        <div className="absolute w-[22vw] pb-5 top-[3rem] left-1/2 translate-x-[-50%] bg-[#143034] z-[100] flex flex-col items-center">
+          <div className="mt-2  flex items-center">
+            <p className="text-2xl text-[white] font-bold">
+              {winner == "tiger_win" ? "Bot Won" : "PLayer Won"}
+            </p>
           </div>
-        }
 
-{
-          (winner == 'tiger_win' || winner == 'goat_win') &&
-          <div  className="absolute w-[22vw] pb-5 top-[3rem] left-1/2 translate-x-[-50%] bg-[#143034] z-[100] flex flex-col items-center">
-
-            <div className="mt-2  flex items-center">
-              <p className="text-2xl text-[white] font-bold">
-                {winner == 'tiger_win' ? "Bot Won" : "PLayer Won"}
-              </p>
-
-            </div>
-
-              <div className="mt-2 flex items-center  gap-2 mt-2">
-                <div>
-                  <div className={`h-[6rem] w-[6rem] rounded flex justify-center items-center bg-[#fff] ${winner == 'tiger_win' ? 'border border-4 border-[#317f41]': 'border border-4 border-[red]'}`}>
-                  <Image
-                    src="/tiger.png"
-                    alt="tiger"
-                    width={50}
-                    height={50}
-                  />
-                  </div>
-                  <div className="w-full flex justify-center">
-                    <p className="text-md text-[white] font-bold">Bot</p>
-                  </div>
-                </div>  
-                    <div>
-                      <p className="text-xl text-[white] font-bold">
-                        vs
-                      </p>
-                    </div>
-                <div>
-                <div className={`h-[6rem] w-[6rem] rounded flex justify-center items-center bg-[#fff] ${winner != 'tiger_win' ? 'border border-4 border-[#317f41]': 'border border-4 border-[red]'}`}>
-                <Image 
-                    src="/goat.png"
-                    alt="tiger"
-                    width={50}
-                    height={50}
-                  />
-                  </div>
-                  <div className="w-full flex justify-center">
-                    <p className="text-md text-[white] font-bold">Player</p>
-                  </div>
-                </div>
+          <div className="mt-2 flex items-center  gap-2 mt-2">
+            <div>
+              <div
+                className={`h-[6rem] w-[6rem] rounded flex justify-center items-center bg-[#fff] ${
+                  winner == "tiger_win"
+                    ? "border border-4 border-[#317f41]"
+                    : "border border-4 border-[red]"
+                }`}
+              >
+                <Image
+                  src="/tiger.png"
+                  alt="tiger"
+                  width={50}
+                  height={50}
+                />
               </div>
+              <div className="w-full flex justify-center">
+                <p className="text-md text-[white] font-bold">Bot</p>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl text-[white] font-bold">vs</p>
+            </div>
+            <div>
+              <div
+                className={`h-[6rem] w-[6rem] rounded flex justify-center items-center bg-[#fff] ${
+                  winner != "tiger_win"
+                    ? "border border-4 border-[#317f41]"
+                    : "border border-4 border-[red]"
+                }`}
+              >
+                <Image
+                  src="/goat.png"
+                  alt="tiger"
+                  width={50}
+                  height={50}
+                />
+              </div>
+              <div className="w-full flex justify-center">
+                <p className="text-md text-[white] font-bold">Player</p>
+              </div>
+            </div>
+          </div>
 
-
-              {/* <div className="mt-2 flex items-center justify-center gap-4 mt-2 h-[4rem] w-[90%]">
+          {/* <div className="mt-2 flex items-center justify-center gap-4 mt-2 h-[4rem] w-[90%]">
                 <p className="text-4xl text-[white] font-bold">755</p>
                 <p className="text-xl text-[red] font-semibold">-11</p>
               </div> */}
 
-
-              <div className="mt-2 flex items-center justify-center gap-4 mt-2 h-[3rem] w-[90%]">
-                
-                <button onClick={()=> window.location.reload()} className="w-[40%] h-[100%] bg-[#317f41] flex justify-center items-center font-bold text-white rounded-[.5rem]">
-                    Rematch
-                </button>
-                <button className="w-[40%] h-[100%] bg-[#317f41] flex justify-center items-center  rounded-[.5rem]">
-                <Link href={'/'} >
-                <p className="font-bold text-white">
-                    Home
-                </p>
-                </Link>
-                </button>
-              </div>
-
-
-              <div className="h-[5rem] w-[22vw] absolute top-[-5] left-0 bg-[black]/70 z-[-1] ">
-              </div>
+          <div className="mt-2 flex items-center justify-center gap-4 mt-2 h-[3rem] w-[90%]">
+            <button
+              onClick={() => window.location.reload()}
+              className="w-[40%] h-[100%] bg-[#317f41] flex justify-center items-center font-bold text-white rounded-[.5rem]"
+            >
+              Rematch
+            </button>
+            <button className="w-[40%] h-[100%] bg-[#317f41] flex justify-center items-center  rounded-[.5rem]">
+              <Link href={"/"}>
+                <p className="font-bold text-white">Home</p>
+              </Link>
+            </button>
           </div>
 
-
-        }
-
+          <div className="h-[5rem] w-[22vw] absolute top-[-5] left-0 bg-[black]/70 z-[-1] "></div>
+        </div>
+      )}
     </div>
   );
 }
